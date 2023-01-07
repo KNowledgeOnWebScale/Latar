f062692ab2dab54f1dd68dd3dc4cbc5d0e81c466
Patrick Hochstenbach <Patrick.Hochstenbach@UGent.be>
2023-01-07 10:21:03 +0100

    Makeltriple

--- alice/alice.pl
+++ alice/alice.pl
@@ -27,41 +27,56 @@ conj_list(A, [A]) :-
 conj_list((A, B), [A|C]) :-
     conj_list(B, C).
 
-% return the level of a statememt
+% an ltriple is a pred(level,subject,object) expression 
+ltriple(T) :-
+   nonvar(T),
+   ltriple(T,_,_,_,_).
+
+ltriple(T,Level,Predicate,Subject,Object) :-
+   nth0(0,L,Predicate),
+   nth0(1,L,Level),
+   nth0(2,L,Subject),
+   nth0(3,L,Object),
+   length(L,4),
+   T =.. L,
+   integer(Level).
+
+% return the level of an l-triple
 level(A,B) :-
-   A =.. L,
-   nth0(1,L,B).
+   ltriple(A,B,_,_,_).
 
-% return the subject of a statement
-subject(A,B) :-
-   A =.. L,
-   nth0(2,L,B).
-
-% return the predicate of a statement
+% return the predicate of an l-triple 
 predicate(A,B) :-
-   A =.. L,
-   nth0(0,L,B).
+   ltriple(A,_,B,_,_).
 
-% return the object of a statement 
+% return the subject of an l-triple
+subject(A,B) :-
+   ltriple(A,_,_,B,_).
+
+% return the object of an l-triple
 object(A,B) :-
-   A =.. L,
-   nth0(3,L,B).
+   ltriple(A,_,_,_,B).
 
-% raise the level of a statement by one
-raise(A,B) :-
-   level(A,X),
-   Y is X + 1,
-   A =.. [F|[_|R]],
-   B =.. [F|[Y|R]].
+% lift the level of a ltriple by one
+lift(A,B) :-
+   nonvar(A),
+   var(B),
+   ltriple(A,L,P,S,O),
+   Ln is L + 1,
+   ltriple(B,Ln,P,S,O).
 
-% lower the level of a statement by one
-lower(A,B) :-
-   level(A,X),
-   Y is X - 1,
-   A =.. [F|[_|R]],
-   B =.. [F|[Y|R]].
+lift(A,B) :-
+   var(A),
+   nonvar(B),
+   ltriple(B,Ln,P,S,O),
+   L is Ln - 1,
+   ltriple(A,L,P,S,O).
 
-% apply a raise or lower to a (nested) statement
+% drop the level of a statement by one
+drop(A,B) :-
+   lift(B,A).
+
+% apply a lift or drop to a (nested) statement
 levelapply(Op,A,B) :-
     % apply the level function on the A statement itself
     Do =.. [Op,A,X] ,
@@ -81,6 +96,38 @@ levelapply(Op,A,B) :-
 
     B =.. [PN,Level,SN,ON].
 
+% create an l-triple from a (possible nested) triple
+makeltriple(Level,A,B) :-
+    integer(Level),
+    nonvar(A),
+    var(B),
+    A =.. L,
+    length(L,3),
+    nth0(0,L,P),
+    nth0(1,L,S),
+    nth0(2,L,O),
+    LevelUp is Level + 1 ,
+    makeltripleG(LevelUp,P,Pn),
+    makeltripleG(LevelUp,S,Sn),
+    makeltripleG(LevelUp,O,On),
+    ltriple(B,Level,Pn,Sn,On).
+
+makeltripleG(Level,A,B) :-
+    conj_list(A,L),
+    makeltripleG(Level,L,[],Ln),
+    reverse(Ln,Lnn),
+    conj_list(B,Lnn).
+
+makeltripleG(_,[],Acc,Acc).
+
+makeltripleG(Level,[H|T],Acc,B) :-
+    ( compound(H) ->
+        makeltriple(Level,H,Hn),
+        makeltripleG(Level,T,[Hn|Acc],B)
+        ;
+        makeltripleG(Level,T,[H|Acc],B)
+    ).
+
 is_odd(A) :-
     nonvar(A),
     atom(A),
@@ -95,6 +142,8 @@ is_even(A) :-
 pos(P,G) :-
     conj_list(G,L),
     insert_procedure(0,P,L).
+
+
 
 insert_procedure(_,_,[]).
 
@@ -119,7 +168,7 @@ erase_prodecure(P,L,LN) :-
 erase_prodecure(_,_,Acc,Acc) .
 
 erase_prodecure(P,[H|T],Acc,LN) :-
-    lower(H,HN),
+    drop(H,HN),
     ( HN -> 
         erase_prodecure(P,T,Acc,LN)  
         ; 
