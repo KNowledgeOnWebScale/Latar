c95bc3c22bb607261dee886a015aec39970ab05e
Patrick Hochstenbach <Patrick.Hochstenbach@UGent.be>
2023-01-21 07:35:20 +0100

    Tests

--- alpha/alice/result.pl
+++ alpha/alice/result.pl
@@ -1,4 +1,2 @@
-GPrime : neg(0,[],(type(1,Alice,Person),neg(1,[],type(2,Alice,Human))))
-GNew : neg(0,[],neg(1,[],type(2,Alice,Human)))
 type(0,'Alice','Person').
 type(0,'Alice','Human').
--- alpha/nested_negation/result.pl
+++ alpha/nested_negation/result.pl
@@ -1,5 +1,1 @@
-GPrime : neg(0,[],neg(1,[],neg(2,[],neg(3,[],type(4,Alice,Human)))))
-GNew : neg(0,[],neg(1,[],neg(2,[],neg(3,[],type(4,Alice,Human)))))
-GPrime : neg(0,[],neg(1,[],type(2,Alice,Human)))
-GNew : neg(0,[],neg(1,[],type(2,Alice,Human)))
 type(0,'Alice','Human').
